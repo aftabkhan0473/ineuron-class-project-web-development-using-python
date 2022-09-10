d42c2ed8142a818b3cf0190dfbcc67e8aac71457
Aftab <therealtopper@gmail.com>
2022-09-10 21:37:07 +0530

    some problem related to while loop solved

--- while_loop.py
+++ while_loop.py
@@ -3,3 +3,24 @@ i = 1;
 while i<=5:
     print("Saurabh Shukla MySirG")
     i += 1;
+
+# 2. Write a program to print first 10 natural number 
+i = 1;
+while i<=10:
+    print(i, end=" ")
+    i += 1;
+print()
+
+# Write a python program to print first 10 natural number in reverse order 
+x=1;
+while x<=10:
+    print(11-x, end=" ");
+    x += 1;
+
+# Write a python program to print first n even natural number 
+print()
+n = int(input("Please Enter how many even numbers you would like to print: "))
+z =1 ;
+while z<=n:
+    print(2*z, end=" ")
+    z += 1;
