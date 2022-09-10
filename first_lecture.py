82b9c5b81bd1eb5182b20516a7c6ca704883e314
Aftab <therealtopper@gmail.com>
2022-09-10 12:38:16 +0530

    added first class project

---
+++ first_lecture.py
@@ -0,0 +1,10 @@
+print("Hello Aftab in the coding world!")
+print("How you feel in this world!")
+print("You have to choose one thing from your takli and this world what you gonna to choose")
+your_choose = int(input("Choose one 1.Takli 2.Coding : "))
+if(your_choose==1):
+    print("You chosen Takli")
+elif(your_choose==2):
+    print("You chosen Coding")
+else:
+    print("Choose only 1 or 2")
